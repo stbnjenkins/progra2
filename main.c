582129ff0f1618ef40a10521f5c983c736b4e570
Esteban Jenkins <stbnjenkins@gmail.com>
2017-05-19 22:33:24 -0600

    commented polygon test in main

--- main.c
+++ main.c
@@ -12,7 +12,9 @@
 #include "first_intersection.c"
 #include "getColor.c"
 #include "ray_tracer.c"
+#include "parser.c"
 #include "frame_manager.c"
+
 #include "keyboard_input.c"
 
 //Run the window
@@ -42,24 +44,24 @@ void window_runner(int resx, int resy) {
 }
 
 int main(int argc, char **argv){
-        POINT p1 = {x: 4.0, y: 4.0, z: 4.0};
-        POINT p2 = {x: 6.0, y: 4.0, z: 4.0};
-        POINT p3 = {x: 6.0, y: 6.0, z: 6.0};
-        POINT p4 = {x: 10.0, y: 2.0, z: 4.0};
-        POINT p5 = {x: 10.0, y: 10.0, z: 10.0};
-        POINT p6 = {x: 12.0, y: 5.0, z: 4.0};
-        POLYGON poly = create_polygon(6, p1,p2,p3,p4,p5,p6);
-        printPolygon(&poly);
+        // POINT p1 = {x: 4.0, y: 4.0, z: 4.0};
+        // POINT p2 = {x: 6.0, y: 4.0, z: 4.0};
+        // POINT p3 = {x: 6.0, y: 6.0, z: 6.0};
+        // POINT p4 = {x: 10.0, y: 2.0, z: 4.0};
+        // POINT p5 = {x: 10.0, y: 10.0, z: 10.0};
+        // POINT p6 = {x: 12.0, y: 5.0, z: 4.0};
+        // POLYGON poly = create_polygon(6, p1,p2,p3,p4,p5,p6);
+        // printPolygon(&poly);
 
-        POLYGON poly2 = create_empty_polygon();
-        addVertex(&poly2, p1);
-        addVertex(&poly2, p2);
-        addVertex(&poly2, p3);
-        addVertex(&poly2, p4);
-        addVertex(&poly2, p5);
-        addVertex(&poly2, p6);
-        put_plane_on_polygon(&poly2);
-        printPolygon(&poly2);
+        // POLYGON poly2 = create_empty_polygon();
+        // addVertex(&poly2, p1);
+        // addVertex(&poly2, p2);
+        // addVertex(&poly2, p3);
+        // addVertex(&poly2, p4);
+        // addVertex(&poly2, p5);
+        // addVertex(&poly2, p6);
+        // put_plane_on_polygon(&poly2);
+        // printPolygon(&poly2);
     // get parameters
     if (argc != 1) {
         printf("Usage <program>\n");
