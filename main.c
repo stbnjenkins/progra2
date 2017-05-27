53e34912a52b37a08fa837b331e90bd4bac9acaa
Esteban Jenkins <stbnjenkins@gmail.com>
2017-05-26 21:44:24 -0600

    removed shit

--- main.c
+++ main.c
@@ -46,26 +46,6 @@ void window_runner() {
 }
 
 int main(int argc, char **argv){
-        //  POINT p1 = {x: 1.0, y: 2.0, z: 3.0};
-        //  POINT2D p2 = get_2d_point_from_3d_point(&p1, 2);
-        //  printf("X = %Lf,\tY = %Lf\n",p2.x,p2.y);
-        // POINT p2 = {x: 6.0, y: 4.0, z: 4.0};
-        // POINT p3 = {x: 6.0, y: 6.0, z: 6.0};
-        // POINT p4 = {x: 10.0, y: 2.0, z: 4.0};
-        // POINT p5 = {x: 10.0, y: 10.0, z: 10.0};
-        // POINT p6 = {x: 12.0, y: 5.0, z: 4.0};
-        // POLYGON poly = create_polygon(6, p1,p2,p3,p4,p5,p6);
-        // printPolygon(&poly);
-
-        // POLYGON poly2 = create_empty_polygon();
-        // addVertex(&poly2, p1);
-        // addVertex(&poly2, p2);
-        // addVertex(&poly2, p3);
-        // addVertex(&poly2, p4);
-        // addVertex(&poly2, p5);
-        // addVertex(&poly2, p6);
-        // put_plane_on_polygon(&poly2);
-        // printPolygon(&poly2);
     // get parameters
     if (argc != 1) {
         printf("Usage <program>\n");
